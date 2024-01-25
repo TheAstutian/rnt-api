6702989d21bd7b1be5be64e431cc24864ef3d1f1
TheAstutian <speakingatoms@gmail.com>
2024-01-25 11:32:05 +0000

    db config update

--- db.js
+++ db.js
@@ -4,6 +4,6 @@ export const db= mysql.createConnection({
     host:process.env.HOST,
     user:process.env.USER,
     password:process.env.PASSWORD,
-    database:process.env.HOST.DATABASE,
+    database:process.env.DATABASE,
     port: process.env.PORT
 })
