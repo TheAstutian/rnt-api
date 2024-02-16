3156726954732879f847947f5c0c6f177111de45
TheAstutian <speakingatoms@gmail.com>
2024-02-16 08:19:09 +0000

    img CDN config

--- index.js
+++ index.js
@@ -4,7 +4,7 @@ import userRoutes from './routes/users.js'
 import postRoutes from './routes/posts.js'
 import cors from 'cors';
 import cookieParser from 'cookie-parser';
-import multer from 'multer'
+
 
 
 
@@ -17,29 +17,6 @@ app.use(cookieParser())
 
 
 
-const storage = multer.diskStorage({
-    destination: function (req, file, cb) {
-      cb(null, '../client/uploads')
-    },
-    filename: function (req, file, cb) {
-      cb(null, Date.now()+file.originalname)
-    }
-  })
-
-const upload = multer({storage: storage})
-
-app.post('/api/upload', upload.single('file'), function (req, res) {  
-  
-  if (req.file){
-    const file=req.file
-    res.status(200).json(file.filename)
-  }
-    else {
-      res.status(200).json("no file sent over to server")
-    }
-    // req.file is the `avatar` file
-    // req.body will hold the text fields, if there were any
-  })
 
 app.use("/api/posts", postRoutes)
 app.use("/api/auth", authRoutes)
